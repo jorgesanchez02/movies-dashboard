c51d18afa9f3fd50668e1a22d291758566225224
Jorge Sanchez <124007824+jorgesanchez02@users.noreply.github.com>
2026-05-30 20:20:45 -0600

    Update app.py

--- app.py
+++ app.py
@@ -7,8 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/15S_wicZzVsEJ0oPEze82Be6eT-52rz48
 """
 
-!pip install streamlit
-
 import streamlit as st
 import pandas as pd
 import firebase_admin
